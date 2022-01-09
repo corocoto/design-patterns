3b7d89bf2847f4152c785dfb7ee14cba8ebfc5e6
Artem Gusev <gusev2014russia@mail.ru>
2022-01-09 17:55:31 +0300

    Adds example of module design pattern

---
+++ packages/module/src/index.js
@@ -0,0 +1,9 @@
+import {add, subtract, divide, multiply, square} from "./math.js";
+
+console.log(add(1, 2));
+console.log(square(3));
+console.log(subtract(5, 4));
+console.log(divide(6, 6));
+console.log(multiply(7, 8));
+
+console.log(privateValue); /* Error: privateValue is not defined */
---
+++ packages/module/src/math.js
@@ -0,0 +1,21 @@
+export function add(x, y) {
+    return x + y;
+}
+
+export function subtract(x, y) {
+    return x - y;
+}
+
+export function multiply(x, y) {
+    return x * y;
+}
+
+export function divide(x, y) {
+    return x / y;
+}
+
+export function square(x) {
+    return x * x
+}
+
+const privateValue = "This variable is private to the module";
